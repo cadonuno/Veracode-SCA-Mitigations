f24a3807ff7b2163299ca0828efb1e847f03fdfd
Ricardo Alvares Pereira <rpereira@veracode.com>
2022-08-11 14:32:40 +0100

    changing how HMAC is signed

--- src/main/java/util/HmacRequestSigner.java
+++ src/main/java/util/HmacRequestSigner.java
@@ -2,15 +2,17 @@ package util;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Locale;
 
+
 public final class HmacRequestSigner {
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
     // Included in the signature to inform Veracode of the signature version.
     private static final String VERACODE_REQUEST_VERSION_STRING = "vcode_request_version_1";
 
@@ -25,9 +27,6 @@ public final class HmacRequestSigner {
 
     // HMAC encryption algorithm.
     private static final String HMAC_SHA_256 = "HmacSHA256";
-
-    // Charset to use when encrypting a string.
-    private static final String UTF_8 = "UTF-8";
 
     // A cryptographically secure random number generator.
     private static final SecureRandom secureRandom = new SecureRandom();
@@ -54,14 +53,15 @@ public final class HmacRequestSigner {
      * @throws InvalidKeyException
      */
     public static String getVeracodeAuthorizationHeader(final ApiCredentials apiCredentials,
-                                                         final URL url, final String httpMethod)
+                                                        final URL url, final String httpMethod)
             throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
         final String urlPath = (url.getQuery() == null)
                 ? url.getPath()
                 : url.getPath().concat("?").concat(url.getQuery());
         final String data = String.format(DATA_FORMAT, apiCredentials.getApiId(), url.getHost(), urlPath, httpMethod);
         final String timestamp = String.valueOf(System.currentTimeMillis());
-        final String nonce = DatatypeConverter.printHexBinary(generateRandomBytes(16)).toLowerCase(Locale.US);
+        byte[] randomBytes = generateRandomBytes(16);
+        final String nonce = bytesToHex(randomBytes).toLowerCase(Locale.US);
         final String signature = getSignature(apiCredentials.getApiKey(), data, timestamp, nonce);
         return String.format(HEADER_FORMAT, VERACODE_HMAC_SHA_256, apiCredentials.getApiId(), timestamp, nonce, signature);
     }
@@ -72,13 +72,13 @@ public final class HmacRequestSigner {
      */
     private static String getSignature(final String key, final String data, final String timestamp, final String nonce)
             throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
-        final byte[] keyBytes = DatatypeConverter.parseHexBinary(key);
-        final byte[] nonceBytes = DatatypeConverter.parseHexBinary(nonce);
+        final byte[] keyBytes = decodeHexString(key);
+        final byte[] nonceBytes = decodeHexString(nonce);
         final byte[] encryptedNonce = hmacSha256(nonceBytes, keyBytes);
         final byte[] encryptedTimestamp = hmacSha256(timestamp, encryptedNonce);
         final byte[] signingKey = hmacSha256(VERACODE_REQUEST_VERSION_STRING, encryptedTimestamp);
         final byte[] signature = hmacSha256(data, signingKey);
-        return DatatypeConverter.printHexBinary(signature).toLowerCase(Locale.US);
+        return printHexBytesAsString(signature);
     }
 
     // Encrypt a string using the provided key.
@@ -86,7 +86,7 @@ public final class HmacRequestSigner {
             throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, UnsupportedEncodingException {
         final Mac mac = Mac.getInstance(HMAC_SHA_256);
         mac.init(new SecretKeySpec(key, HMAC_SHA_256));
-        return mac.doFinal(data.getBytes(UTF_8));
+        return mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
     }
 
     // Encrypt a byte array using the provided key.
@@ -102,5 +102,51 @@ public final class HmacRequestSigner {
         final byte[] key = new byte[size];
         secureRandom.nextBytes(key);
         return key;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
+    }
+
+    public static byte[] decodeHexString(String hexString) {
+        if (hexString.length() % 2 == 1) {
+            throw new IllegalArgumentException(
+                    "Invalid hexadecimal String supplied.");
+        }
+
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
+        }
+        return bytes;
+    }
+
+    public static byte hexToByte(String hexString) {
+        int firstDigit = toDigit(hexString.charAt(0));
+        int secondDigit = toDigit(hexString.charAt(1));
+        return (byte) ((firstDigit << 4) + secondDigit);
+    }
+
+    private static int toDigit(char hexChar) {
+        int digit = Character.digit(hexChar, 16);
+        if(digit == -1) {
+            throw new IllegalArgumentException(
+                    "Invalid Hexadecimal Character: "+ hexChar);
+        }
+        return digit;
+    }
+
+    public static String printHexBytesAsString(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte aByte : bytes) {
+            result.append(String.format("%02x", aByte));
+        }
+        return result.toString();
     }
 }
